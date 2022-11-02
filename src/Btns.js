138d0eccfd56f343db2da3dc3d4f84861fb0a360
chkim0 <kpes2002@naver.com>
2022-11-02 11:24:02 +0900

    moveUp함수 완성

--- src/Btns.js
+++ src/Btns.js
@@ -1,34 +1,44 @@
-import React from 'react'
-
-function Btns({panel}) {
-
-const getItems = item => {
-    const panel = panel.current;
-    const panel_items = panel.children;
+function Btns({ panel }) {
+  const getItems = item => {
+    const panel_ul = item.current;
+    const panel_items = panel_ul.children;
     const items = Array.from(panel_items);
     const len = items.length;
-    const current_item = panel.querySelector('on');
+    const current_item = panel_ul.querySelector('.on');
     const current_index = items.indexOf(current_item);
-  
-    return [panel, panel_items, len, current_item, current_index];
+    return [panel_ul, panel_items, len, current_item, current_index];
   }
 
-  const moveUp = ()=>{
-    const [panel, panel_items, len, current_item, current_index] = getItems(panel);
+  const moveUp = () => {
+    const [panel_ul, panel_items, len, current_item, current_index] = getItems(panel);
+    let next_index = null;
+    (current_index !== len - 1) ? (next_index = current_index + 1) : (next_index = 0);
+
+    current_item.classList.remove('on');
+    current_item.classList.add('up');
+    panel_items[next_index].classList.add('down');
+
+    setTimeout(() => {
+      panel_items[next_index].classList.remove('down');
+      panel_items[next_index].classList.add('on');
+      panel_ul.querySelector('.up').classList.remove('up');
+    }, 800)
   }
   return (
     <>
-    <a href='#' className='btnUp' onClick={moveUp}>
+      <a href="#" className="btnUp" onClick={moveUp}>
         <span></span>
         <em>UP</em>
-    </a>
-    <a href='#' className='btnDown'>
+      </a>
+
+      <a href="#" className="btnDown">
         <span></span>
         <em>DOWN</em>
-    </a>
-
+      </a>
     </>
-  )
+  );
 }
 
-export default Btns
+
+
+export default Btns;
